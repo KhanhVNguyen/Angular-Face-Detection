02026ee0b34d7e8de849c806ddd6d3e7deb3a927
KhanhNguyen <nvkhanh1775@gmail.com>
2019-08-10 11:35:39 +0700

    open video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as faceapi from '../libraries/face-api.min.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'face-detection';
+  @ViewChild('video', { static: false }) video: ElementRef;
+
+  constructor() {
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    ])
+
+  }
+  startVideo() {
+    var browser = <any>navigator;
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia({ video: true, audio: false }).then(stream => {
+      this.video.nativeElement.srcObject = stream;
+      this.video.nativeElement.play();
+    })
+  }
 }
